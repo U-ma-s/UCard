dac8c2cbbe5da3eca4be723ee8d9d34a6711f23b
U-ma-s <yuma.prodia4989@gmail.com>
2023-02-22 16:35:16 +0900

    Initial Commit

---
+++ UCard/UCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  UCardApp.swift
+//  UCard
+//
+//  Created by 杉下友麻 on 2023/02/22.
+//
+
+import SwiftUI
+
+@main
+struct UCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
